49570b2bdda58861c1830163985c27c0d4a83ccb
fnitu <fnitu@users.noreply.github.com>
2018-09-04 00:21:09 +0300

    add links

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,12 @@ const IndexPage = () => (
   <div>
     <h1>Working like a quacking duck!</h1>
     <a href="https://fnitu.github.io/">Homepage</a>
+    <br/>
+    <a href="https://github.com/fnitu">Github</a>
+    <br/>
+    <a href="https://www.freecodecamp.org/fnitu">FreeCodeCamp</a>
+    <br/>
+    <a href="https://medium.com/@florin.n.nitu">Medium</a>
   </div>
 )
 
