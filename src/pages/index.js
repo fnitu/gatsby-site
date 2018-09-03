10fa78fa8f81845940689e2ea7163be0b08c7203
fnitu <fnitu@users.noreply.github.com>
2018-09-04 00:00:11 +0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Working like a quacking duck!</h1>
+    <a href="https://fnitu.github.io/">Homepage</a>
+  </div>
+)
+
+export default IndexPage
